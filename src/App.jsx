7b442bd35053172043d53dbc22ca2b9e87ea46bc
Henri K <12832691+nkahe@users.noreply.github.com>
2024-02-20 15:59:47 +0200

    filtteröinnin lisäämiseen asti

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+
+  const [listedPersons, setListedPersons] = useState(persons);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  const addName = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    }
+
+    const personExists = persons.some(person => person.name === newName);
+
+    if (personExists) {
+      alert(`${newName} is already in phonebook`);
+      return;
+    }
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const changeFilter = (event) => {
+    const filter = event.target.value;
+    const newListedPersons = persons.filter(person =>
+      person.name.toLowerCase().includes(filter));
+    setListedPersons(newListedPersons);
+  }
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+
+      <p>
+        filter shown with <input onChange={changeFilter}/>
+      </p>
+
+      <h2>Add new</h2>
+
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input  onChange={(event) => setNewName(event.target.value)}
+                  value={newName}
+          /><br />
+          puh:
+          <input onChange={(event) => setNewNumber(event.target.value)}
+                 value={newNumber}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      
+        {
+          listedPersons.map((person) => (
+            <p key={person.name}>{person.name}, {person.number}
+            </p>
+          ))
+        }
+
+      {/* <div>debug: {newName}</div> */}
+    </div>
+  )
+
+}
+
+export default App
